b4bb30e2136bd078b84f30bd4787bcd1d1dab024
LeonardsonCC <leonardson.carvalho@gmail.com>
2020-10-22 17:38:03 -0300

    everything working

---
+++ Recorder.py
@@ -0,0 +1,83 @@
+from array import array
+import pyaudio
+import wave
+
+def record_audio(WAVE_OUTPUT_FILE):
+    #--------- SETTING PARAMS FOR OUR AUDIO FILE ------------#
+    FORMAT = pyaudio.paInt16    # format of wave
+    CHANNELS = 2                # no. of audio channels
+    RATE = 44100                # frame rate
+    CHUNK = 1024                # frames per audio sample
+    THRESHOLD = 50
+    #--------------------------------------------------------#
+     
+    # creating PyAudio object
+    audio = pyaudio.PyAudio()
+     
+    # open a new stream for microphone
+    # It creates a PortAudio Stream Wrapper class object
+    stream = audio.open(format=FORMAT,channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
+
+
+    #----------------- start of recording -------------------#
+    print("Listening...")
+
+    # list to save all audio frames
+    frames = []
+
+    # verify how many times just passed 5 seconds
+    iterator_count = 0
+    iterator_stop = False
+
+    while True:
+        for i in range(int(RATE / CHUNK * 5)):
+            if iterator_count > 3:
+                iterator_stop = True
+                break
+
+            # read audio stream from microphone
+            data = stream.read(CHUNK)
+
+            if max(array('h', data)) < THRESHOLD and iterator_count > 2:
+                print(i, "SILENCIO POR MUITO TEMPO PARANDO GRAVACAO")
+                iterator_stop = True
+                break
+
+            # append audio data to frames list
+            frames.append(data)
+
+        if iterator_stop is True:
+            break
+
+        iterator_count += 1
+
+    #------------------ end of recording --------------------#   
+    print("Finished recording.")
+      
+    stream.stop_stream()    # stop the stream object
+    stream.close()          # close the stream object
+    audio.terminate()       # terminate PortAudio
+
+    #------------------ saving audio ------------------------#
+
+    # create wave file object
+    waveFile = wave.open(WAVE_OUTPUT_FILE, 'wb')
+
+    # settings for wave file object
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+
+    # closing the wave file object
+    waveFile.close()
+    return WAVE_OUTPUT_FILE
+
+
+def read_audio(WAVE_FILE):
+    # function to read audio(wav) file
+    WAVE_FILE.seek(0)
+    audio = WAVE_FILE.read()
+    return audio
---
+++ speech_to_text.py
@@ -0,0 +1,52 @@
+import threading
+import requests
+import json
+import io
+import configparser
+from Recorder import record_audio, read_audio
+
+# Getting config parameters
+config = configparser.ConfigParser()
+config.read("config.ini")
+API_ENDPOINT = config['WIT_AI']['API_ENDPOINT']
+wit_access_token = config['WIT_AI']['ACCESS_TOKEN']
+
+def start_record_audio():
+    # record audio of specified length in specified audio file
+    temp_file = io.BytesIO()
+    audio_bytes = record_audio(temp_file)
+    
+    # reading audio
+    audio = read_audio(audio_bytes)
+
+    thread_text_from_audio = threading.Thread(target=get_text_from_audio, args={audio: audio})
+    thread_text_from_audio.start()
+
+
+def get_text_from_audio(audio):
+    # defining headers for HTTP request
+    headers = {'authorization': 'Bearer ' + wit_access_token,
+               'Content-Type': 'audio/wav'}
+
+    # making an HTTP post request
+    resp = requests.post(API_ENDPOINT, headers = headers,
+                         data = audio)
+    
+    # converting response content to JSON format
+    data = json.loads(resp.content)
+    
+    # get text from data
+    text = data['text'] if data['text'] else data['_text']
+    
+    # return the text
+    write_text_to_file(text, "text.txt")
+
+
+def write_text_to_file(text, filename):
+    with open(filename, 'a') as text_file:
+        text_file.write(text)
+    print("finished write")
+
+if __name__ == "__main__":
+    while True:
+        audio = start_record_audio()
